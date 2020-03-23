c315ed3e7fe718115c67ef288d22a5b276d61a10
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-03-23 11:48:34 +0000

    finished taxonomy command

--- src/Taxonomies.php
+++ src/Taxonomies.php
@@ -23,6 +23,75 @@ defined( 'ABSPATH' ) || exit;
 class Taxonomies extends DirectoryStackCommand {
 
 	/**
+	 * Generate terms for a specific listing taxonomy.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     $ wp ds taxonomies generate --taxonomy=listing_category --number=10
+	 *
+	 * @param array $args command arguments.
+	 * @param array $assoc_args command arguments.
+	 * @return void
+	 */
+	public function generate( $args, $assoc_args ) {
+
+		$r = wp_parse_args(
+			$assoc_args,
+			array(
+				'taxonomy' => false,
+				'number'   => 10,
+			)
+		);
+
+		$taxonomies = Admin::get_registered_listings_taxonomies();
+
+		if ( ! array_key_exists( $r['taxonomy'], $taxonomies ) ) {
+			WP_CLI::error( 'Taxonomy does not belong to the listing post type.' );
+		}
+
+		$notify = \WP_CLI\Utils\make_progress_bar( 'Generating terms for ' . $taxonomies[ $r['taxonomy'] ], $r['number'] );
+
+		foreach ( range( 1, $r['number'] ) as $index ) {
+			if ( $r['taxonomy'] === 'listing_location' ) {
+				wp_insert_term( \Faker\Provider\en_US\Address::country(), $r['taxonomy'] );
+			} else {
+				wp_insert_term( \Faker\Provider\Base::lexify( 'Term ?????' ), $r['taxonomy'] );
+			}
+
+			$notify->tick();
+		}
+		$notify->finish();
+
+		if ( $r['taxonomy'] === 'listing_location' || $r['taxonomy'] === 'listing_category' ) {
+			$terms = get_terms(
+				array(
+					'taxonomy'   => $r['taxonomy'],
+					'hide_empty' => false,
+					'number'     => 9999,
+				)
+			);
+
+			$random_terms = \Faker\Provider\Base::randomElements( $terms, 3 );
+
+			foreach ( $random_terms as $term ) {
+
+				$cats_amount = 5;
+				$notify      = \WP_CLI\Utils\make_progress_bar( 'Generating random child terms for ' . $term->name, $cats_amount );
+
+				foreach ( range( 1, $cats_amount ) as $index ) {
+					wp_insert_term( \Faker\Provider\Base::numerify( 'Child Type ###' ), $r['taxonomy'], array( 'parent' => $term->term_id ) );
+					$notify->tick();
+				}
+				$notify->finish();
+
+			}
+		}
+
+		WP_CLI::success( 'Taxonomy terms successfully generated.' );
+
+	}
+
+	/**
 	 * Reset all terms of a specific listing taxonomy.
 	 *
 	 * ## EXAMPLES
